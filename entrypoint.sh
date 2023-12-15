6a433b86935f3d9940ecf0f193fe61a80b56fe1c
Huamin Chen <hchen@redhat.com>
2023-12-15 12:29:27 -0500

    make license consistent
    Signed-off-by: Huamin Chen <hchen@redhat.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,7 @@
 #!/usr/bin/env bash
 #
 # This file is part of the Kepler project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-#     http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Copyright 2023 The Kepler Contributors
-#
-
-# This script creates a spot instance request and waits for it to become ready.
-# It then outputs the instance ID of the created instance.
-#
-# The script requires the AWS CLI to be installed and configured.
+# See LICENSE file for license information.
 
 # shellcheck disable=SC1000-SC9999
 
